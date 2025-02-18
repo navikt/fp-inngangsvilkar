bdc70114622ddd70b86ef3c0476d46cab5ee4ae9
Anders Palfi <anders.palfi@nav.no>
2025-02-18 13:50:18 +0100

    TFP-5982 bruker personidenttype for å utlede avvik i medlemskap framfor å se på personstatus dnr (#30)

--- src/main/java/no/nav/foreldrepenger/inngangsvilkaar/regelmodell/medlemskap/v2/SjekkOmBosattPersonstatus.java
+++ src/main/java/no/nav/foreldrepenger/inngangsvilkaar/regelmodell/medlemskap/v2/SjekkOmBosattPersonstatus.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.inngangsvilkaar.regelmodell.medlemskap.v2;
 
 import static no.nav.foreldrepenger.inngangsvilkaar.regelmodell.medlemskap.v2.Personopplysninger.PersonstatusPeriode.Type.BOSATT_ETTER_FOLKEREGISTERLOVEN;
+import static no.nav.foreldrepenger.inngangsvilkaar.regelmodell.medlemskap.v2.Personopplysninger.PersonstatusPeriode.Type.D_NUMMER;
 import static no.nav.foreldrepenger.inngangsvilkaar.regelmodell.medlemskap.v2.Personopplysninger.PersonstatusPeriode.Type.DØD;
 
 import java.util.List;
@@ -12,7 +13,14 @@ import no.nav.fpsak.nare.specification.LeafSpecification;
 @RuleDocumentation(SjekkOmBosattPersonstatus.ID)
 class SjekkOmBosattPersonstatus extends LeafSpecification<MedlemskapsvilkårMellomregning> {
 
-    private static final List<Personopplysninger.PersonstatusPeriode.Type> GYLDIGE_STATUSER = List.of(BOSATT_ETTER_FOLKEREGISTERLOVEN, DØD);
+    // Fra PDL-dokumentasjon om folkreregisterpersonstatus;
+    // "Siden statusen er sterkt knyttet til Folkeregisterets lovverk og saksbehandling,
+    // anbefaler vi PDLs konsumenter å være svært varsomme med å bruke denne opplysningstypen.
+    // Folkeregisterpersonstatus bør dermed ikke benyttes for å avgjøre vilkår"
+    // Ser derfor på identtype når det kommer til dnummer
+
+    private static final List<Personopplysninger.PersonstatusPeriode.Type> GYLDIGE_STATUSER = List.of(BOSATT_ETTER_FOLKEREGISTERLOVEN, DØD,
+        D_NUMMER);
     static final String ID = "FP_VK_2-3";
 
     private static final String BESKRIVELSE = "Har bruker bosatt personstatus?";
@@ -26,7 +34,7 @@ class SjekkOmBosattPersonstatus extends LeafSpecification<MedlemskapsvilkårMell
         var grunnlag = mellomregning.grunnlag();
         var personopplysninger = grunnlag.personopplysninger();
         var vurderingsperiode = grunnlag.vurderingsperiodeBosatt();
-        if (!MedlemFellesRegler.sjekkOmBosattPersonstatus(GYLDIGE_STATUSER, personopplysninger, vurderingsperiode)) {
+        if (!MedlemFellesRegler.sjekkOmBosattPersonstatus(GYLDIGE_STATUSER, personopplysninger, vurderingsperiode) || personopplysninger.personIdentType() == Personopplysninger.PersonIdentType.DNR) {
             mellomregning.addAvvik(MedlemskapAvvik.BOSATT_UGYLDIG_PERSONSTATUS);
         }
         return ja();
--- src/main/java/no/nav/foreldrepenger/inngangsvilkaar/regelmodell/medlemskap/v2/SjekkOmOppholdstillatelserIHeleTredjelandsPerioden.java
+++ src/main/java/no/nav/foreldrepenger/inngangsvilkaar/regelmodell/medlemskap/v2/SjekkOmOppholdstillatelserIHeleTredjelandsPerioden.java
@@ -1,13 +1,8 @@
 package no.nav.foreldrepenger.inngangsvilkaar.regelmodell.medlemskap.v2;
 
-import java.util.Set;
-
 import no.nav.fpsak.nare.doc.RuleDocumentation;
 import no.nav.fpsak.nare.evaluation.Evaluation;
 import no.nav.fpsak.nare.specification.LeafSpecification;
-import no.nav.fpsak.tidsserie.LocalDateInterval;
-import no.nav.fpsak.tidsserie.LocalDateSegment;
-import no.nav.fpsak.tidsserie.LocalDateTimeline;
 
 @RuleDocumentation(SjekkOmOppholdstillatelserIHeleTredjelandsPerioden.ID)
 class SjekkOmOppholdstillatelserIHeleTredjelandsPerioden extends LeafSpecification<MedlemskapsvilkårMellomregning> {
@@ -30,15 +25,5 @@ class SjekkOmOppholdstillatelserIHeleTredjelandsPerioden extends LeafSpecificati
         }
 
         return ja();
-    }
-
-    private static LocalDateTimeline<Personopplysninger.Region> regionTimelineIVurderingsperiode(Set<Personopplysninger.RegionPeriode> regioner,
-                                                                                                 LocalDateInterval vurderingsperiode) {
-        var regionSegmenter = regioner.stream()
-            .filter(srp -> srp.region().equals(Personopplysninger.Region.TREDJELAND))
-            .map(rp -> new LocalDateSegment<>(rp.periode(), rp.region()))
-            .toList();
-
-        return new LocalDateTimeline<>(regionSegmenter).intersection(vurderingsperiode);
     }
 }
